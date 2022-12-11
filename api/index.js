79774ae825706c9069344cd65160b8007a4b61a1
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2022-12-11 16:03:35 +0100

    Authentication register/ login

--- api/index.js
+++ api/index.js
@@ -36,4 +36,4 @@ ConnectDB();
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT} 🚀`));

c29509af51fa94269ac23a66d0ffc6e86d6d0ba7
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2022-12-21 21:35:31 +0100

    verify token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,12 @@
 import express from "express"
 import { updateUser, deleteUser, getUser, getUsers } from "../controller/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
+})
 
 //~ Update
 router.put("/:id",updateUser)

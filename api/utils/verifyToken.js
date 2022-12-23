563c80f698ac27436bee536c7f7d84130ba53e66
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2022-12-23 17:53:25 +0100

    mongodb model relation

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -10,12 +10,12 @@ export const verifyToken = (req, res, next) => {
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
-    next();
+    next(); 
   })
 }
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, ()=> {
+  verifyToken(req, res,next, ()=> {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     next();
   } else {

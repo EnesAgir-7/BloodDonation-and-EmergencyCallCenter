08d32d4a6cc812cea69f2b5d48694a1a9a426847
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2022-12-24 17:44:44 +0100

    header design

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import { Header } from '../../component/header/Header';
+import Navbar from '../../component/navbar/Navbar';
 import './home.css';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Header />
+    </div>
   )
 }
